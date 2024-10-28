7b0abacd1bb605c6819684a9b029f2284bcd7d56
Erik Holmgren <erikholmgren24@gmail.com>
2024-10-28 16:35:55 +0100

    Utility function for pre-computing cluster AR and precip values for e.g. plotting.

--- analysis/ar_scandinavia/utils.py
+++ analysis/ar_scandinavia/utils.py
@@ -2,7 +2,9 @@
 
 import logging
 
+import numpy as np
 import xarray as xr
+from distributed.client import Client
 from xr_utils.coordinates import conv_lon_to_180
 
 logger = logging.getLogger(__name__)
@@ -33,3 +35,45 @@ def subsel_ds(
     ).chunk("auto")
 
     return sel_ds
+
+
+def compute_ar_pr_values(
+    ar_ds: xr.Dataset,
+    precip_ds: xr.Dataset,
+    path: str,
+    n_clusters: int,
+    resample_id: str,
+    client: Client,
+) -> tuple[list[xr.DataArray], list[xr.DataArray]]:
+    """Compute AR and PR values for each cluster id. Useful for plotting."""
+    cluster_ds = xr.open_zarr(path).load().labels
+    cluster_labels, cluster_counts = np.unique(cluster_ds, return_counts=True)
+
+    ar_vals = []
+    pr_vals = []
+    for cluster_id in range(n_clusters):
+        ar_vals.append(
+            xr.where(
+                ar_ds.resample(time=resample_id)
+                .sum()
+                .isel(time=cluster_ds == cluster_id)
+                .ar_unique_id
+                > 0,
+                1,
+                0,
+            ).sum("time")
+        )
+        pr_vals.append(
+            precip_ds.resample(time=resample_id)
+            .sum()
+            .isel(time=cluster_ds == cluster_id)
+            .tp.sum("time")
+        )
+
+    ar_vals = client.compute(ar_vals)
+    pr_vals = client.compute(pr_vals)
+
+    ar_vals = [ar_val.result() for ar_val in ar_vals]
+    pr_vals = [pr_val.result() for pr_val in pr_vals]
+
+    return ar_vals, pr_vals
